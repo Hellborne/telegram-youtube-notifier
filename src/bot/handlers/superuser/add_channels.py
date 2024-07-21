c0d5cfb32cc179aa3d05c4877fdd9a9c20535db0
Vlad <mailbyvlad@gmail.com>
2024-07-21 18:26:02 +0800

    Channel list

--- src/bot/handlers/superuser/add_channels.py
+++ src/bot/handlers/superuser/add_channels.py
@@ -1,6 +1,7 @@
 from io import TextIOWrapper
 from typing import BinaryIO
 from typing import Optional
+from datetime import datetime, timedelta
 
 from aiogram import Bot
 from aiogram import F
@@ -23,7 +24,7 @@ from ...states import ChannelsSG
 from src.utils import youtube_channel_url_validator
 
 add_channels_router = Router(name="add_channels")
-
+last_command_time = {}
 
 @add_channels_router.message(
     Command("add_channels"),
@@ -33,10 +34,10 @@ add_channels_router = Router(name="add_channels")
 async def add_channels(message: Message, state: FSMContext, **kwargs) -> None:
     await message.answer(
         text="Upload a file with format: <br/>"
-        "<code>url[TAB]label[END_ROW]</code><br/>"
-        "every single line == channel to insert.<br/>"
-        "Set channel url in format: <b>https://www.youtube.com/@username</b>"
-        "Enter /cancel for exit <br/>",
+             "<code>url[TAB]label[END_ROW]</code><br/>"
+             "every single line == channel to insert.<br/>"
+             "Set channel url in format: <b>https://www.youtube.com/@username</b>"
+             "Enter /cancel for exit <br/>",
         parse_mode=SULGUK_PARSE_MODE,
     )
     await state.set_state(ChannelsSG.bulk_channels)
@@ -48,7 +49,7 @@ async def add_channels(message: Message, state: FSMContext, **kwargs) -> None:
     StateFilter(ChannelsSG.bulk_channels),
 )
 async def channel_file_handler(
-    message: Message, state: FSMContext, bot: Bot, dal: DataAccessLayer, **kwargs
+        message: Message, state: FSMContext, bot: Bot, dal: DataAccessLayer, **kwargs
 ) -> None:
     file_id = message.document.file_id
     file: File = await bot.get_file(file_id=file_id)
@@ -78,7 +79,7 @@ async def channel_file_handler(
                 if not youtube_channel_url_validator(splitted_line[0]):
                     await message.answer(
                         text=f"Error url validation: {splitted_line[0]} <br/>"
-                        "Set channel url in format: <b>https://www.youtube.com/@username</b>",
+                             "Set channel url in format: <b>https://www.youtube.com/@username</b>",
                         parse_mode=SULGUK_PARSE_MODE,
                     )
                     return
@@ -99,14 +100,41 @@ async def channel_file_handler(
 
     await state.clear()
 
+
 @add_channels_router.message(
     Command("list_channels", "каналы"),
     State(state="*"),
 )
 async def list_channels(message: Message, dal: DataAccessLayer, bot: Bot, **kwargs) -> None:
+    user_id = message.from_user.id
+    current_time = datetime.now()
+
+    # Check if the user is a superuser
+    user_schema = await dal.get_user_by_attr(**{"user_id": user_id})
+    if user_schema and user_schema.is_superuser:
+        # Superusers are not subject to rate limiting
+        await send_channel_list(message, dal, bot)
+        return
+
+    # Check if the user has called the command within the last 30 minutes
+    if user_id in last_command_time:
+        last_time = last_command_time[user_id]
+        if current_time - last_time < timedelta(minutes=1):
+            await bot.send_message(
+                chat_id=user_id,
+                text="Частота запросов 1 раз в минуту."
+            )
+            return
+
+    # Update the last command time for the user
+    last_command_time[user_id] = current_time
+
+    await send_channel_list(message, dal, bot)
+
+async def send_channel_list(message: Message, dal: DataAccessLayer, bot: Bot) -> None:
     channels = await dal.get_channels()
     if not channels:
-        await message.answer("No channels found.")
+        await message.answer("Каналы не найдены.")
         return
 
     response = "Список отслеживаемых каналов:\n"
@@ -114,4 +142,5 @@ async def list_channels(message: Message, dal: DataAccessLayer, bot: Bot, **kwar
         response += f"{channel.label}: {channel.url}\n"
 
     await bot.send_message(chat_id=message.from_user.id, text=response)
+
 __all__ = ["add_channels_router"]
